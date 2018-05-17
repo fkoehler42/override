0fa849904e1e891019dc536e930feaddb6460804
Flavien <fkoehler@student.42.fr>
2018-05-17 20:00:45 +0200

    level3 update source

--- level03/source.c
+++ level03/source.c
@@ -3,33 +3,93 @@
 #include <string.h>
 #include <time.h>
 
-void		decrypt(int n) {
-	char	s1[] = "Q}|u";			// 0x757c7d51
-	char	s2[] = "`sfg";			// 0x67667360
-	char	s3[] = "~sfg";			// 0x7b66737e
-	char	s4[] = "}|a3";			// 0x33617c7d
-	char	c = '\0';
+void		decrypt(int mask) {
+	int				str = 0x757c7d51;	// "Q}|u"
+	int				str2 = 0x67667360;	// "`sfg"
+	int				str3 = 0x7b66737e;	// "~sfg"
+	int				str4 = 0x33617c7d;	// "}|a3"
+	char			c = 0;
+	unsigned int	str_len = strlen((char*)&str);
+	int				i = 0;
+	char			*tmp;
 
-	int s1_len;
-	int	whatever;
-
-	s1_len = strlen(s1);
-	whatever = 0;
-	s1 + s1_len;
-
-	if (strncmp(s1, "Congratulations!", 17) == 0)
+	while (i < str_len) {
+		tmp = (char*)&str + i;
+		*tmp = (int)*tmp ^ mask;
+		i++;
+	}
+	if (strncmp((char*)&str, "Congratulations!", 17) == 0)
 		system("/bin/sh");
 	else
 		puts("\nInvalid Password");
 }
 
 void		test(int password, int value) {
-	value -= password;
-	if ((size_t)value <= 21) {
-	//	value = value << 2;
-		decrypt(value);
-	} else {
-		decrypt(rand());
+	int		test_value;
+
+	switch (value - password) {
+		case 1: {
+			decrypt(1);
+			break;
+		}
+		case 2: {
+			decrypt(2);
+			break;
+		}
+		case 3: {
+			decrypt(3);
+			break;
+		}
+		case 4: {
+			decrypt(4);
+			break;
+		}
+		case 5: {
+			decrypt(5);
+			break;
+		}
+		case 6: {
+			decrypt(6);
+			break;
+		}
+		case 7: {
+			decrypt(7);
+			break;
+		}
+		case 8: {
+			decrypt(8);
+			break;
+		}
+		case 9: {
+			decrypt(9);
+			break;
+		}
+		case 16: {
+			decrypt(16);
+			break;
+		}
+		case 17: {
+			decrypt(17);
+			break;
+		}
+		case 18: {
+			decrypt(18);
+			break;
+		}
+		case 19: {
+			decrypt(19);
+			break;
+		}
+		case 20: {
+			decrypt(20);
+			break;
+		}
+		case 21: {
+			decrypt(21);
+			break;
+		}
+		default :
+			decrypt(rand());
 	}
 }
 
@@ -37,11 +97,14 @@ int			main(void) {
 	int		password;
 
 	srand(time(NULL));
+
 	puts("***********************************");
 	puts("*\t\tlevel03\t\t**");
 	puts("***********************************");
+
 	printf("Password:");
 	scanf("%d", &password);
-	test(password, 322424845);
+	test(password, 0x1337d00d);	// 322424845
+	
 	return (0);
 }
