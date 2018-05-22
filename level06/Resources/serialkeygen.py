bfcd3284d2be9f6b0b35e31f0b50efa32ecfb4eb
moannuo <ademenet@e2r9p11.42.fr>
2018-05-22 14:14:48 +0200

    Serial keygen works fine

--- level06/Resources/serialkeygen.py
+++ level06/Resources/serialkeygen.py
@@ -1,22 +1,24 @@
+"""
+Little serial keygen. Example: keygen("AAAAAA") should give you 6229050.
+"""
+import argparse
 import struct
 
 def keygen(login):
     assert(len(login) > 5 and len(login) <= 32)
     cst = (ord(login[3]) ^ 0x1337) + 0x5eeded
-    # print "cst = ", cst
     for letter in login:
         if ord(letter) <= 31:
             break
         val1 = ord(letter) ^ cst
-        print "val1 = ", val1
-        val2 = (0x88233b2b * val1) / 0x100000000
-        print "val2 = ", val2
-        val3 = val1 + (-1337) * ((val1 - val2) / 2 + val2) / 1024 + cst
-        print "val3 = ", val3
+        val2 = 0x88233b2b * val1 / 0x100000000
+        val3 = val1 + ((val1 - val2) / 2 + val2) / 1024 * -1337 + cst
         cst = val3
-        # print "new cst = ", cst
-    print val3
+    print "The serial corresponding to " + str(login) + " is: " + str(val3)
 
-keygen("AAAAAA")
-# 6229050
-# struct.unpack('>i', bytes('9DA92DAB', 'utf-8').decode('hex'))
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Little serial keygen.')
+    parser.add_argument('login', type=str, help='login must be between 5 and 32 characters')
+    args = vars(parser.parse_args())
+    keygen(args["login"])
