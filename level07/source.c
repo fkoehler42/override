cd1d29c59aab09cfb12c9c79219329b671f5436b
Flavien <fkoehler@student.42.fr>
2018-05-28 13:50:12 +0200

    level07 source done

--- level07/source.c
+++ level07/source.c
@@ -2,21 +2,39 @@
 #include <stdio.h>
 #include <string.h>
 
-int         clear_stdin()
+void        clear_argv(char **argv)
 {
-    
+    while (*argv)
+    {
+        memset(*argv, 0, strlen(*argv));
+        argv++;
+    }
 }
 
-int         get_unum()
+void        clear_envp(char **envp) { clear_argv(envp); }
+
+void        clear_stdin(void)
 {
-    int number;
+    char x = 0;
+    while (42)
+    {
+        x = getchar();
+        if (x == '\n' || x == EOF)	
+        break;
+	}
+}
+
+int         get_unum(void)
+{
+    unsigned int    number;
+
     fflush(stdin);
-    scanf("%u", (unsigned int)number);
+    scanf("%u", &number);
     clear_stdin();
     return number;
 }
 
-int store_number(unsigned int * data)
+int         store_number(unsigned int *data)
 {
     unsigned int input = 0;
     unsigned int index = 0;
@@ -34,25 +52,20 @@ int store_number(unsigned int * data)
 
         return 1;
     }
-
     data[index] = input;
-
     return 0;
 }
 
-
-int read_number(unsigned int * data) {
+int         read_number(unsigned int *data) {
     unsigned int index = 0;
 
     printf(" Index: ");
     index = get_unum();
-
     printf(" Number at data[%u] is %u\n", index, data[index]);
-
     return 0;
 }
 
-int main(int argc, char * argv[], char * envp[]) {
+int         main(int argc, char **argv, char **envp) {
     int res = 0;
     char cmd[20] = {0};
     unsigned int data[100] = {0};
@@ -72,13 +85,9 @@ int main(int argc, char * argv[], char * envp[]) {
            "----------------------------------------------------\n"\
            "\n");
 
-
     while(1) {
-
         printf("Input command: ");
         res = 1;
-
-
         fgets(cmd, sizeof(cmd), stdin);
         cmd[strlen(cmd)-1] = '\0';
 
@@ -88,7 +97,6 @@ int main(int argc, char * argv[], char * envp[]) {
             res = read_number(data);
         else if(!strncmp(cmd, "quit", 4))
             break;
-
         if(res)
             printf(" Failed to do %s command\n", cmd);
         else
@@ -96,6 +104,5 @@ int main(int argc, char * argv[], char * envp[]) {
 
         memset(cmd, 0, sizeof(cmd));
     }
-
     return EXIT_SUCCESS;
 }
